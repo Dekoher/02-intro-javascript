6ca393814d7a57dd8f0ffb62c10e43726003f930
diegoher <diegoherr92@gmail.com>
2020-08-18 23:29:39 -0500

    inital commit ordering files structure

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,22 @@
+
+// variables and constants
+
+const name = 'diego';
+const lastName = 'herrera';
+
+let givenValue = 5;
+givenValue = 2;
+
+console.log(name, lastName, givenValue);
+
+// var MUSTN'T be used anymore
+
+if (true) {
+  let givenValue = 6;
+  console.log(givenValue);
+  
+  const name = 'Peter';
+  console.log(name);
+}
+
+console.log(givenValue);
